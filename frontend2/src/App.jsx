1483ab4756e5a2e8a699f3dc92bcb9fb949f1854
harshchoudhary21 <harshchoudhary210j@gmail.com>
2025-02-11 00:16:02 +0530

    Frontend

---
+++ frontend2/src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+
+import './App.css'
+import Login from './components/auth/login'
+
+
+function App() {
+ 
+
+  return (
+    <div className="App" >
+       App
+        <Login />
+       
+
+      </div>
+      
+  )
+}
+
+export default App
--- frontend2/src/main.jsx
+++ frontend2/src/main.jsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import SignIn from './features/auth/sign-in/SignIn'
+import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <SignIn />
+    <App />
   </StrictMode>,
 )
